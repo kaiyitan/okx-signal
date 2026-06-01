8690a694085eb3bda0638a6de87939ffa98932bc
kaiyi <tankaiyi123@gmail.com>
2026-06-01 22:46:51 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,11 @@ echo "=== Pulling latest code ==="
 git pull
 
 echo "=== Building and starting services ==="
-docker-compose -f docker-compose.prod.yml down
-docker-compose -f docker-compose.prod.yml build --no-cache
-docker-compose -f docker-compose.prod.yml up -d
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml build --no-cache
+docker compose -f docker-compose.prod.yml up -d
 
 echo "=== Status ==="
-docker-compose -f docker-compose.prod.yml ps
+docker compose -f docker-compose.prod.yml ps
 
 echo "=== Done. API is running on http://$(curl -s ifconfig.me) ==="
